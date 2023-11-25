a9bf0d26d7b4477129c3b227b64c0a9ce171676b
unknown <you@example.com>
2023-11-25 15:16:19 -0800

    my update part

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-// document.getElementById('calls').addEventListener('click', function(){
-//     document.getElementById('con').innerHTML = document.getElementById('hed').innerHTML
-// })
-
-// document.getElementById('m-ch').addEventListener('click', function(){
-//     document.getElementById('con').innerHTML = document.getElementById('chat-container').innerHTML
-// })
+function updates(){
+    document.getElementById('con').innerHTML = document.getElementById('meet').innerHTML
+}
 
 function calls(){
     document.getElementById('con').innerHTML = document.getElementById('hed').innerHTML
