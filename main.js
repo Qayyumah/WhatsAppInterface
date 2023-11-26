3216207ab4843e1f1e5c08f327ae3d865d4d0a61
unknown <you@example.com>
2023-11-26 02:16:29 -0800

    my community part

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ function calls(){
     document.getElementById('con').innerHTML = document.getElementById('hed').innerHTML
 }
 
+function people(){
+    document.getElementById('con').innerHTML = document.getElementById('last').innerHTML
+
+}
+
 document.getElementById('m-ch').addEventListener('click', function(){
     document.getElementById('con').innerHTML = document.getElementById('chat-container').innerHTML
 })
